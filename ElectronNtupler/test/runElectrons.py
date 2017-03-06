83386a3ac4ecc5eeaa4ac479bf401064fad013d6
ridhi chawla <ridhi.chawla@cern.ch>
2017-03-06 14:17:40 +0100

    updated .py file

--- ElectronNtupler/test/runElectrons.py
+++ ElectronNtupler/test/runElectrons.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Ntupler")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi");
 process.load("Geometry.CaloEventSetup.CaloGeometry_cfi");
@@ -12,17 +12,26 @@ process.load("Geometry.CaloEventSetup.CaloTopology_cfi");
 #
 # Define input data to read
 #
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 #process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.GlobalTag.globaltag = 'MCRUN2_74_V9::All'                                             #MC
 #process.GlobalTag.globaltag = 'GR_E_V49::All'                                                 #Data
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
-#process.GlobalTag.globaltag = cms.string("74X_dataRun2_v4")
-#process.GlobalTag.globaltag = cms.string("74X_dataRun2_reMiniAOD_v0")                                     #Data
-#process.GlobalTag.globaltag = cms.string("74X_dataRun2_Prompt_v4")
-process.GlobalTag.globaltag = cms.string("74X_mcRun2_asymptotic_v2")                                      #MC
+#process.GlobalTag.globaltag = cms.string("76X_mcRun2_asymptotic_v12")
+process.GlobalTag.globaltag = cms.string("76X_dataRun2_v15")
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+        calibratedPatElectrons = cms.PSet(
+	initialSeed = cms.untracked.uint32(1),
+	engineName = cms.untracked.string('TRandom3')
+	),
+	calibratedElectrons = cms.PSet(
+	initialSeed = cms.untracked.uint32(1),
+	engineName = cms.untracked.string('TRandom3')
+	),
+	                                           )
 
 inputFilesAOD = cms.untracked.vstring(
     # AOD test files from /DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/AODSIM
@@ -31,20 +40,13 @@ inputFilesAOD = cms.untracked.vstring(
 
 inputFilesMiniAOD = cms.untracked.vstring(
     # MiniAOD test files from /DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM
+
+       #'/store/mc/RunIIFall15MiniAODv2/DYJetsToLL_M-2000to3000_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext1-v1/40000/1EE52939-48D8-E511-8D0A-20CF3019DF17.root',
+       
+       '/store/data/Run2015D/SingleElectron/MINIAOD/16Dec2015-v1/20001/86E53624-32A7-E511-A28D-0025905A60D0.root',
+       #'/store/data/Run2015C_25ns/SinglePhoton/MINIAOD/16Dec2015-v1/50000/02E07C2B-90B2-E511-96BB-0CC47A78A3F4.root'
     
-    #'/store/data/Run2015D/SingleElectron/MINIAOD/PromptReco-v4/000/258/159/00000/0EC56452-186C-E511-8158-02163E0146D5.root'
-    #'/store/data/Run2015D/SingleElectron/MINIAOD/05Oct2015-v1/10000/00991D45-4E6F-E511-932C-0025905A48F2.root'
-    
-    #'/store/data/Run2015D/MuonEG/MINIAOD/PromptReco-v4/000/258/159/00000/64914E6C-F26B-E511-B0C8-02163E0142D1.root'
-    #'/store/data/Run2015D/MuonEG/MINIAOD/05Oct2015-v2/60000/00D43A12-C573-E511-8F4C-0025905A60E0.root'
-    
-    #'/store/mc/RunIISpring15MiniAODv2/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/10000/2699AF1F-9F6D-E511-BDD8-0025901D4AF0.root'
-    '/store/mc/RunIISpring15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/50000/041705A6-6F6F-E511-AC9C-001E6757F1D4.root'
-    #'/store/mc/RunIISpring15MiniAODv2/DYJetsToLL_M-500to700_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/60000/C057FAA9-456C-E511-BA7D-0025905C42FE.root'
-    #'/store/mc/RunIISpring15MiniAODv2/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/008865AA-596D-E511-92F1-0025905A6110.root'
-    #'/store/mc/RunIISpring15MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/00087FEB-236E-E511-9ACB-003048FF86CA.root'
-    
-    )
+)
 
 #
 # You can list here either AOD or miniAOD files, but not both types mixed
@@ -58,10 +60,11 @@ else :
     print("MiniAOD input files are used")
 process.source = cms.Source ("PoolSource", fileNames = inputFiles )                             
 
+process.load('EgammaAnalysis.ElectronTools.calibratedElectronsRun2_cfi')
+
 #
 # Set up electron ID (VID framework)
 #
-
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
 # turn on VID producer, indicate data format  to be
 # DataFormat.AOD or DataFormat.MiniAOD, as appropriate 
@@ -81,35 +84,49 @@ my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElect
 for idmod in my_id_modules:
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
+process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('calibratedPatElectrons')
+process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('selectedElectrons')
+
 #
 # Configure the ntupler module
 #
+process.selectedElectrons = cms.EDFilter("PATElectronSelector",
+src = cms.InputTag("slimmedElectrons"),
+cut = cms.string("pt > 10 && abs(eta)<2.5")
+)
+
+# should have no effect on slimmedElectrons
+#process.calibratedPatElectrons.isMC = cms.bool(True)
 
 process.ntupler = cms.EDAnalyzer('SimpleElectronNtupler',
                                  # The module automatically detects AOD vs miniAOD, so we configure both
                                  #
                                  # Common to all formats objects
                                  #
-                                 isMC     = cms.untracked.bool(True),
-				 isSIG    = cms.untracked.bool(True),
+                                 isMC     = cms.untracked.bool(False),
+				 isSIG    = cms.untracked.bool(False),
 				 trigger  = cms.InputTag("TriggerResults::HLT"),
 				 prescale = cms.InputTag("patTrigger"),
 				 #pileup   = cms.InputTag("addPileupInfo"),
                                  pileup   = cms.InputTag("slimmedAddPileupInfo"),
 				 rho      = cms.InputTag("fixedGridRhoFastjetAll"),
                                  beamSpot = cms.InputTag('offlineBeamSpot'),
+                                 eventWeight   = cms.InputTag("generator"),
                                  #
                                  # Objects specific to AOD format
                                  #
-                                 electrons    = cms.InputTag("gedGsfElectrons"),
-                                 genParticles = cms.InputTag("genParticles"),
+                                 #electrons    = cms.InputTag("gedGsfElectrons"),
+                                 electrons    = cms.InputTag("calibratedElectrons"),
+				 genParticles = cms.InputTag("genParticles"),
                                  vertices     = cms.InputTag("offlinePrimaryVertices"),
                                  conversions  = cms.InputTag('allConversions'),
                                  #
                                  # Objects specific to MiniAOD format
                                  #
                                  muonsMiniAOD    = cms.InputTag("slimmedMuons"),
-				 electronsMiniAOD    = cms.InputTag("slimmedElectrons"),
+				 #electronsMiniAOD    = cms.InputTag("slimmedElectrons"),
+				 #electronsMiniAOD    = cms.InputTag("selectedElectrons"),
+				 electronsMiniAOD    = cms.InputTag("calibratedPatElectrons"),
 				 photonsMiniAOD      = cms.InputTag("slimmedPhotons"),
 				 metsMiniAOD         = cms.InputTag("slimmedMETs"),
                                  genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
@@ -127,18 +144,11 @@ process.ntupler = cms.EDAnalyzer('SimpleElectronNtupler',
                                  phoMediumIdMap = cms.InputTag("egmPhotonIDs:cutBasedPhotonID-Spring15-25ns-V1-standalone-medium"),
                                  phoTightIdMap = cms.InputTag("egmPhotonIDs:cutBasedPhotonID-Spring15-25ns-V1-standalone-tight"),
 				 #
-                                 # ValueMaps with MVA results
-                                 #
-                                 #mvaValuesMap     = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Phys14NonTrigValues"),
-                                 #mvaCategoriesMap = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Phys14NonTrigCategories"),
 				 # This is a fairly verbose mode if switched on, with full cut flow 
 				 # diagnostics for each candidate. Use it in a low event count test job.
 				 eleMediumIdFullInfoMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-medium",),
 				 eleIdVerbose = cms.bool(False),
-				 
 				 objects = cms.InputTag('selectedPatTrigger')
-				 #tagFilterName   = cms.vstring("hltEle23Ele12CaloIdTrackIdIsoTrackIsoLeg1Filter"),
-				 #probeFilterName = cms.vstring("hltEle23Ele12CaloIdTrackIdIsoTrackIsoLeg2Filter")
                                  )
 
 process.primaryVertexFilter  = cms.EDFilter("VertexSelector",
@@ -147,11 +157,27 @@ process.primaryVertexFilter  = cms.EDFilter("VertexSelector",
       filter = cms.bool(True)  ## otherwise it won't filter the events, just produce an empty vertex collection.
       )
 
+process.hcalDDDRecConstants = cms.ESProducer( "HcalDDDRecConstantsESModule",
+  appendToDataLabel = cms.string( "" )
+)
+process.hcalDDDSimConstants = cms.ESProducer( "HcalDDDSimConstantsESModule",
+  appendToDataLabel = cms.string( "" )
+)
+
+#from PhysicsTools.PatAlgos.preselection_eltau_cff import *
+#process.leadingElectron = process.slimmedElectrons.clone(
+#    src = 'slimmedElectrons', cut = 'pt>20'
+#    )
+#process.load("PhysicsTools.PatAlgos.selectionLayer1.electronCountFilter_cfi")
+#process.leadingElectronRequirement = process.countPatElectrons.clone(minNumber = 1, src = 'leadingElectron')
+#process.leadingleptonsequence = cms.Sequence(process.leadingElectron+process.leadingElectronRequirement)
+
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('DY_check.root')
-				   #fileName = cms.string('SE_Run2015C_v1.root')
+                                   fileName = cms.string('Data_76X.root')
+				   #fileName = cms.string('Data.root')
                                    )
 
 
-process.p = cms.Path(process.egmGsfElectronIDSequence * process.primaryVertexFilter * process.ntupler)
-#process.p = cms.Path(process.egmPhotonIDSequence * process.ntupler)
+process.p = cms.Path(process.selectedElectrons + process.calibratedPatElectrons + process.egmGsfElectronIDSequence + process.primaryVertexFilter + process.ntupler)
+#process.p = cms.Path(process.selectedElectrons * process.egmGsfElectronIDSequence * process.primaryVertexFilter * process.ntupler)
+#process.p = cms.Path(process.egmGsfElectronIDSequence * process.primaryVertexFilter * process.ntupler)
